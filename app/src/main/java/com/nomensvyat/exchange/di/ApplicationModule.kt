bc5e464194ebee64afc6272b7bdda6fcd5e284b0
Svyatoslav Chernov <nomen.svyat@gmail.com>
2019-06-14 13:39:28 +0300

    Fix issue with module annotation

--- app/src/main/java/com/nomensvyat/exchange/di/ApplicationModule.kt
+++ app/src/main/java/com/nomensvyat/exchange/di/ApplicationModule.kt
@@ -17,6 +17,7 @@ class ApplicationModule(private val application: Application) {
     fun provideContext(): Context = application
 
 
+    @Module
     abstract class Bindings {
 
         @Singleton
